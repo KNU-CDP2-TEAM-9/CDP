303a83d5c63ac953dc4816313c27c5327351b606
insung2 <84179188+2insung@users.noreply.github.com>
2023-04-28 20:48:10 +0900

    fix(database) : change database and add curField state (in Contents.js)

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const mysql = require("mysql");
+const { checkAuth } = require("../util/auth");
 const router = express.Router();
+const dbconfig = require("../config/database");
+const connection = mysql.createConnection(dbconfig);
+
+router.use(checkAuth);
 
 router.post("/", async (req, res, next) => {
   const data = req.body;
-  const response = data.body;
-  console.log(data);
-  res.json(response);
+  const sql = "insert into user_chat set ?";
+  const chat = {
+    user_id: "",
+    field_index: "",
+    chat_message: "",
+    isUser: "",
+    chatDate: "",
+  };
 });
 
 module.exports = router;
--- frontend/src/component/Contents.js
+++ frontend/src/component/Contents.js
@@ -1,17 +1,24 @@
+import React, { useState } from "react";
 import classes from "./Contents.module.css";
 import ChatUI from "./Chat";
 import BottomUI from "./Bottom";
 import SideUI from "./Side";
 
 const Contents = () => {
+  const [curField, setCurField] = useState(0);
+
+  const curFieldHandler = (cur) => {
+    setCurField(cur);
+  };
+
   return (
     <div className={classes.wrapper}>
       <div className={classes.primary}>
-        <ChatUI />
+        <ChatUI field={curField} />
         <BottomUI />
       </div>
       <div className={classes.secondary}>
-        <SideUI />
+        <SideUI onChangeField={curFieldHandler} />
       </div>
     </div>
   );
