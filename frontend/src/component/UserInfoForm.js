0ad1959cfb809963fdf9e9253ae695af915d97fe
jjsshhhhh <jshinkor00@naver.com>
2023-05-18 01:31:35 +0900

    design: memo & userInfo 수정

--- frontend/src/component/UserInfoForm.js
+++ frontend/src/component/UserInfoForm.js
@@ -52,126 +52,151 @@ const UserInfoForm = (props) => {
   };
   return (
     <>
-      nickName
-      {isEditing.nickName === false ? (
-        <div id="nickName" onClick={ClickHandler}>
-          {props.userInfo.nickName ? props.userInfo.nickName : "----"}
-        </div>
-      ) : (
-        <form action="#" id="nickName" onSubmit={SubmitHandler}>
-          <input
-            className={classes.Input}
-            type="text"
-            name="Input"
-            defaultValue={props.userInfo.nickName}
-          ></input>
-        </form>
-      )}
-      firstName
-      {isEditing.firstName === false ? (
-        <div id="firstName" onClick={ClickHandler}>
-          {props.userInfo.firstName ? props.userInfo.firstName : "----"}
-        </div>
-      ) : (
-        <form action="#" id="firstName" onSubmit={SubmitHandler}>
-          <input
-            className={classes.Input}
-            type="text"
-            name="Input"
-            defaultValue={props.userInfo.firstName}
-          ></input>
-        </form>
-      )}
-      lastName
-      {isEditing.lastName === false ? (
-        <div id="lastName" onClick={ClickHandler}>
-          {props.userInfo.lastName ? props.userInfo.lastName : "----"}
-        </div>
-      ) : (
-        <form action="#" id="lastName" onSubmit={SubmitHandler}>
-          <input
-            className={classes.Input}
-            type="text"
-            name="Input"
-            defaultValue={props.userInfo.lastName}
-          ></input>
-        </form>
-      )}
-      dept
-      {isEditing.dept === false ? (
-        <div id="dept" onClick={ClickHandler}>
-          {props.userInfo.dept ? props.userInfo.dept : "----"}
-        </div>
-      ) : (
-        <form action="#" id="dept" onSubmit={SubmitHandler}>
-          <input
-            className={classes.Input}
-            type="text"
-            name="Input"
-            defaultValue={props.userInfo.dept}
-          ></input>
-        </form>
-      )}
-      phoneNumber
-      {isEditing.phoneNumber === false ? (
-        <div id="phoneNumber" onClick={ClickHandler}>
-          {props.userInfo.phoneNumber ? props.userInfo.phoneNumber : "----"}
-        </div>
-      ) : (
-        <form action="#" id="phoneNumber" onSubmit={SubmitHandler}>
-          <input
-            className={classes.Input}
-            type="text"
-            name="Input"
-            defaultValue={props.userInfo.phoneNumber}
-          ></input>
-        </form>
-      )}
-      grade
-      {isEditing.grade === false ? (
-        <div id="grade" onClick={ClickHandler}>
-          {props.userInfo.grade ? props.userInfo.grade : "----"}
-        </div>
-      ) : (
-        <form action="#" id="grade" onSubmit={SubmitHandler}>
-          <input
-            className={classes.Input}
-            type="text"
-            name="Input"
-            defaultValue={props.userInfo.grade}
-          ></input>
-        </form>
-      )}
-      earned_credit
-      {isEditing.earned_credit === false ? (
-        <div id="earned_credit" onClick={ClickHandler}>
-          {props.userInfo.earned_credit ? props.userInfo.earned_credit : "----"}
-        </div>
-      ) : (
-        <form action="#" id="earned_credit" onSubmit={SubmitHandler}>
-          <input
-            className={classes.Input}
-            type="text"
-            name="Input"
-            defaultValue={props.userInfo.earned_credit}
-          ></input>
-        </form>
-      )}
-      goal_credit
-      {isEditing.goal_credit === false ? (
-        <div id="goal_credit" onClick={ClickHandler}>
-          {props.userInfo.goal_credit ? props.userInfo.goal_credit : "----"}
-        </div>
-      ) : (
-        <form action="#" id="goal_credit" onSubmit={SubmitHandler}>
-          <input
-            className={classes.Input}
-            type="text"
-            name="Input"
-            defaultValue={props.userInfo.goal_credit}
-          ></input>
-        </form>
-      )}
+    <div className={classes.wrapper}>
+      <div className={classes.wrapper_info}>
+        <div className={classes.label_info}>nickName</div>
+          {isEditing.nickName === false ? (
+            <div className={classes.text_info} id="nickName" onClick={ClickHandler}>
+              {props.userInfo.nickName ? props.userInfo.nickName : "----"}
+            </div>
+          ) : (
+            <form action="#" id="nickName" onSubmit={SubmitHandler}>
+              <input
+                className={classes.Input}
+                type="text"
+                name="Input"
+                defaultValue={props.userInfo.nickName}
+              ></input>
+            </form>
+          )}
+      </div>
+      
+      <div className={classes.wrapper_info}>
+        <div className={classes.label_info}>firstName</div>
+          {isEditing.firstName === false ? (
+            <div className={classes.text_info}  id="firstName" onClick={ClickHandler}>
+              {props.userInfo.firstName ? props.userInfo.firstName : "----"}
+            </div>
+          ) : (
+            <form action="#" id="firstName" onSubmit={SubmitHandler}>
+              <input
+                className={classes.Input}
+                type="text"
+                name="Input"
+                defaultValue={props.userInfo.firstName}
+              ></input>
+            </form>
+          )}
+      </div>
+      
+      <div className={classes.wrapper_info}>
+        <div className={classes.label_info}>lastName</div>
+          {isEditing.lastName === false ? (
+            <div className={classes.text_info}  id="lastName" onClick={ClickHandler}>
+              {props.userInfo.lastName ? props.userInfo.lastName : "----"}
+            </div>
+          ) : (
+            <form action="#" id="lastName" onSubmit={SubmitHandler}>
+              <input
+                className={classes.Input}
+                type="text"
+                name="Input"
+                defaultValue={props.userInfo.lastName}
+              ></input>
+            </form>
+          )}
+      </div>
+      
+      <div className={classes.wrapper_info}>
+        <div className={classes.label_info}>dept</div>
+          {isEditing.dept === false ? (
+            <div className={classes.text_info}  id="dept" onClick={ClickHandler}>
+              {props.userInfo.dept ? props.userInfo.dept : "----"}
+            </div>
+          ) : (
+            <form action="#" id="dept" onSubmit={SubmitHandler}>
+              <input
+                className={classes.Input}
+                type="text"
+                name="Input"
+                defaultValue={props.userInfo.dept}
+              ></input>
+            </form>
+          )}
+      </div>
+      
+      <div className={classes.wrapper_info}>
+        <div className={classes.label_info}>phoneNumber</div>
+          {isEditing.phoneNumber === false ? (
+            <div className={classes.text_info}  id="phoneNumber" onClick={ClickHandler}>
+              {props.userInfo.phoneNumber ? props.userInfo.phoneNumber : "----"}
+            </div>
+          ) : (
+            <form action="#" id="phoneNumber" onSubmit={SubmitHandler}>
+              <input
+                className={classes.Input}
+                type="text"
+                name="Input"
+                defaultValue={props.userInfo.phoneNumber}
+              ></input>
+            </form>
+          )}
+      </div>
+      
+      <div className={classes.wrapper_info}>
+        <div className={classes.label_info}>grade</div>
+          {isEditing.grade === false ? (
+            <div className={classes.text_info}  id="grade" onClick={ClickHandler}>
+              {props.userInfo.grade ? props.userInfo.grade : "----"}
+            </div>
+          ) : (
+            <form action="#" id="grade" onSubmit={SubmitHandler}>
+              <input
+                className={classes.Input}
+                type="text"
+                name="Input"
+                defaultValue={props.userInfo.grade}
+              ></input>
+            </form>
+          )}
+      </div>
+      
+      <div className={classes.wrapper_info}>
+        <div className={classes.label_info}>earned_credit</div>
+          {isEditing.earned_credit === false ? (
+            <div className={classes.text_info}  id="earned_credit" onClick={ClickHandler}>
+              {props.userInfo.earned_credit ? props.userInfo.earned_credit : "----"}
+            </div>
+          ) : (
+            <form action="#" id="earned_credit" onSubmit={SubmitHandler}>
+              <input
+                className={classes.Input}
+                type="text"
+                name="Input"
+                defaultValue={props.userInfo.earned_credit}
+              ></input>
+            </form>
+          )}
+      </div>
+      
+      <div className={classes.wrapper_info}>
+        <div className={classes.label_info}>goal_credit</div>
+          {isEditing.goal_credit === false ? (
+            <div className={classes.text_info}  id="goal_credit" onClick={ClickHandler}>
+              {props.userInfo.goal_credit ? props.userInfo.goal_credit : "----"}
+            </div>
+          ) : (
+            <form action="#" id="goal_credit" onSubmit={SubmitHandler}>
+              <input
+                className={classes.Input}
+                type="text"
+                name="Input"
+                defaultValue={props.userInfo.goal_credit}
+              ></input>
+            </form>
+          )}
+      </div>
+    </div>
     </>
   );
 };
