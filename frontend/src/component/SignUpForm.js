b74ceb024d7196c0495838c5ada66ae96ce46928
jjsshhhhh <jshinkor00@naver.com>
2023-05-17 23:21:46 +0900

    design: signup 페이지 수정

--- frontend/src/component/SignUpForm.js
+++ frontend/src/component/SignUpForm.js
@@ -9,30 +9,39 @@ const SignUpForm = () => {
   return (
     <>
       <Form method="post">
-        <h1>Create a new user</h1>
-        {data && data.errors && (
-          <ul>
-            {Object.values(data.errors).map((err) => {
-              return <li key={err}>{err}</li>;
-            })}
-          </ul>
-        )}
-        <p>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="text" name="email" required />
-        </p>
-        <p>
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" required />
-        </p>
-        <p>
-          <label htmlFor="nickName">nickName</label>
-          <input id="nickName" type="text" name="nickName" required />
-        </p>
-        <div>
-          <button disabled={isSubmitting}>
-            {isSubmitting ? "Submitting ..." : "Save"}
-          </button>
+        <div className={classes.wrapper_create}>
+          {/* <h1>Create a new user</h1> */}
+          <div className={classes.img_logo}></div>
+          {data && data.errors && (
+            <ul>
+              {Object.values(data.errors).map((err) => {
+                return <li key={err}>{err}</li>;
+              })}
+            </ul>
+          )}
+          <p>
+            {/* <label htmlFor="email">Email</label> */}
+            <input className={classes.input_email} 
+              placeholder="email"
+              id="email" type="text" name="email" required />
+          </p>
+          <p>
+            {/* <label htmlFor="password">Password</label> */}
+            <input className={classes.input_pw} 
+              placeholder="password" 
+              id="password"type="password" name="password" required />
+          </p>
+          <p>
+            {/* <label htmlFor="nickName">nickName</label> */}
+            <input className={classes.input_nick} 
+              placeholder="nickName" 
+              id="nickName"type="text" name="nickName" required />
+          </p>
+          <div>
+            <button className={classes.btn_save} disabled={isSubmitting}>
+              {isSubmitting ? "Submitting ..." : "Create User"}
+            </button>
+          </div>
         </div>
       </Form>
     </>
